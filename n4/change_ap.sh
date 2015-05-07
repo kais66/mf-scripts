62d696330a09e6c49e22f62bce51eded6784b146
Kai Su <kais@winlab.rutgers.edu>
2015-05-07 16:08:36 -0400

    updates

--- n4/change_ap.sh
+++ n4/change_ap.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
+
 wlan_if=wlan0
-ap=mf_trans_ap_1
-chann=11
+ap=mf_trans_ap_2
+chann=1
 ip=192.168.3.2
 ap_ip=192.168.3.1
-iwconfig $wlan_if mode Managed essid $ap channel $chann 
-ifconfig $wlan_if $ip up
-route add -net 192.168.0.0 netmask gw $ap_ip
+#iwconfig $wlan_if mode Managed essid $ap channel $chann commit
+iwconfig $wlan_if essid $ap channel $chann commit
+echo "iwconfig $wlan_if mode Managed essid $ap channel $chann"
+
+#ifconfig $wlan_if $ip up
+#route add -net 192.168.0.0 netmask gw $ap_ip

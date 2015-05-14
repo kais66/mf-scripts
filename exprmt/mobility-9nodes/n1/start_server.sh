cb45b45c35d98ec90f7e66d9169da84c4bbdb16d
Francesco Bronzino <bronzino@winlab.rutgers.edu>
2015-05-14 14:51:23 -0400

    Update to client scripts

--- exprmt/mobility-9nodes/n1/start_server.sh
+++ exprmt/mobility-9nodes/n1/start_server.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
+killall -9 mfstack
 /root/mobilityfirst/mfclient/hoststack/src/mfstack receiver_settings -d > /var/log/mf/stack.log 2> /var/log/mf/stack.log &
 /root/mobilityfirst/mfapps/http-apps/mfproxy/mfproxy -m -P 80 -t 20
--- exprmt/mobility-9nodes/n9/start_proxy.sh
+++ exprmt/mobility-9nodes/n9/start_proxy.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
+killall -9 mfstack
 /root/mobilityfirst/mfclient/hoststack/src/mfstack sender_settings -d > /var/log/mf/stack.log 2> /var/log/mf/stack.log &
 /root/mobilityfirst/mfapps/http-apps/mfproxy/mfproxy -p -P 80 -t 20 -s 1
